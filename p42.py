556bb86a3d683d9cfcf996c01cfea9423f6b00a5
afarica <afaricanistan@gmail.com>
2019-11-12 05:02:11 +0600

    extra3.2

---
+++ p42.py
@@ -0,0 +1,9 @@
+a=float(input("Enter the degrees:"))
+b=input("What to translate?(enter with F or C):")
+if b=="C": 
+	d=5/9*(a-32)
+	print("In °C it will be equated ",d)
+elif b=="F":
+	s=9/5*a+32
+	print("In °F it will be equated ",s)
+
